32feeddabf116ad2c28e037512eb439f0cae0919
Peter Ade-Ojo <adeojopeter@gmail.com>
2023-03-23 23:22:26 +0100

    fix for conspirators data

--- pages/home/index.js
+++ pages/home/index.js
@@ -23,3 +23,31 @@ export default function Home({ conspirators }) {
     </>
   );
 }
+
+Home.defaultProps = {
+  conspirators: [
+    {
+      name: "@boluwatifee__",
+      thumbnail: "https://ui-avatars.com/api/?name=Boluwatife",
+      image: "https://ui-avatars.com/api/?name=Boluwatife",
+      description: "Boluwatife is a 21 year old self-taught software engineer. He is passionate about the arts and is the founder of C-65. He is also a student at the University of Lagos where he is studying Computer Engineering.",
+      link: "https://twitter.com/boluwatifee__",
+    },
+    {
+      name: "@identity.id",
+      thumbnail: "https://ui-avatars.com/api/?name=Amanda+Charles",
+      description: "Charles is a 17 year old student who is passionate about the arts. He is a rapper and a singer who is currently working on his first EP. He is also a photographer and a graphic designer. He is the founder of C-65 and is passionate about providing a platform for the youth to express themselves through the arts.",
+      link: "https://instagram.com/identity.id"
+    },
+    {
+      name: "@pluto_c65",
+      thumbnail: "https://ui-avatars.com/api/?name=Pluto",
+      description: "Pluto is a 17 year old student who is passionate about the arts. He is a rapper and a singer who is currently working on his first EP. He is also a photographer and a graphic designer. He is the founder of C-65 and is passionate about providing a platform for the youth to express themselves through the arts.",
+    },
+    {
+      name: "@tomi",
+      thumbnail: "https://ui-avatars.com/api/?name=Tomi",
+      description: "Pluto is a 17 year old student who is passionate about the arts. He is a rapper and a singer who is currently working on his first EP. He is also a photographer and a graphic designer. He is the founder of C-65 and is passionate about providing a platform for the youth to express themselves through the arts.",
+    },
+  ],
+}
--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,15 @@ import Home from "./home";
 
 export default Home;
 
-export async function getStaticProps() {
-  const url = `${process.env.API_URL}/api/conspirators`;
-  console.log(url);
-  const res = await fetch(`${process.env.API_URL}/api/conspirators`);
-  const conspirators = await res.json();
+// export async function getStaticProps() {
+//   const url = `${process.env.API_URL}/api/conspirators`;
+//   console.log(url);
+//   const res = await fetch(`${process.env.API_URL}/api/conspirators`);
+//   const conspirators = await res.json();
 
-  return {
-    props: {
-      conspirators,
-    },
-  };
-}
+//   return {
+//     props: {
+//       conspirators,
+//     },
+//   };
+// }
